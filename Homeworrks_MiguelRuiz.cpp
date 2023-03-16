b4619379982017d5cde9c35666f5769e82ee20f9
Elinmaculado <miguel.ruiz@uniat.edu.mx>
2023-03-16 10:56:10 -0600

    First commit

---
+++ Homeworrks_MiguelRuiz.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int guessmynumber();
+
+int main()
+{
+    int excercise;
+
+    cout << "elija el numero de trabajo a ver: ";
+    cin >> excercise;
+    switch (excercise) {
+    case 1:
+        system("cls");
+        guessmynumber();
+        break;
+    case 2:
+        cout << "aun no existe el trabajo 2";
+    }
+}
+
+int guessmynumber() {
+    srand(static_cast<unsigned int>(time(0)));
+    //srand(time(NULL));
+    int randomNumber = rand();
+    int secretNumber = (randomNumber % 200) + 1;
+    int guess;
+    int tries = 0;
+    int veryClose;
+
+    cout << "GUESS MY NUMBER" << endl;
+    cout << "Adivina el número en el menor número de intentos posible." << endl;
+
+    //Depurar
+    cout << secretNumber;
+    do
+    {
+        cout << "\nEnter a guess (1-200):" << endl;
+
+        cin >> guess;
+        tries++;
+
+        veryClose = secretNumber - guess;
+
+        if (veryClose <= 5 && veryClose >= -5 && veryClose != 0)
+        {
+            cout << "Muy cerca!!\n";
+        }
+
+        if (guess > secretNumber) {
+            cout << "Muy Alto\n\n";
+        }
+        else if (guess < secretNumber) {
+            cout << "Muy Bajo\n\n";
+        }
+        else {
+            cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
+
+        }
+    } while (guess != secretNumber);
+    return 0;
+}
